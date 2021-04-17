706d0fe3b61f50be59a30ff8b132802380a86a83
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-04-17 17:32:53 -0300

    Bloco 5, dia 2, Passo 1

---
+++ Modulo_1/bloco_5/dia_2/main.js
@@ -0,0 +1,11 @@
+//Ex1;
+
+const elementH1 = document.createElement("h1");
+elementH1.innerHTML = "Exercício 5.2 - JavaScript DOM";
+document.body.appendChild(elementH1);
+
+// Ex2
+
+const divMainContent = createElement("div");
+divMainContent.classList.add("main-content");
+document.body.appendChild(divMainContent);
