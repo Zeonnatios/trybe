bde2200f2a861611b7e37a4616e89e12df07a8bd
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-04-17 21:07:27 -0300

    Gabarito exercicios restantes

--- Modulo_1/bloco_5/dia_3/script.js
+++ Modulo_1/bloco_5/dia_3/script.js
@@ -181,3 +181,67 @@ function newTaskDiv(color) {
 }
 
 newTaskDiv("pink");
+
+function setTaskClass() {
+  let selectedTask = document.getElementsByClassName("task selected");
+  let myTasks = document.querySelector(".task");
+
+  myTasks.addEventListener("click", (event) => {
+    if (selectedTask.length === 0) {
+      event.target.className = "task selected";
+    } else {
+      event.target.className = "task";
+    }
+  });
+}
+
+setTaskClass();
+
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName("task selected");
+  let days = document.querySelector("#days");
+  let taskDiv = document.querySelector(".task");
+  let taskColor = taskDiv.style.backgroundColor;
+
+  days.addEventListener("click", function (event) {
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = "rgb(119,119,119)";
+    }
+  });
+}
+
+setDayColor();
+
+function addNewTask() {
+  let getInputField = document.querySelector("#task-input");
+  let addInputButton = document.querySelector("#btn-add");
+  let getTaskList = document.querySelector(".task-list");
+
+  addInputButton.addEventListener("click", function () {
+    if (getInputField.value.length > 0) {
+      let newLi = document.createElement("li");
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = "";
+    } else {
+      alert("Error: Digite ao menos 1 caractere.");
+    }
+  });
+
+  getInputField.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13 && getInputField.value.length > 0) {
+      let newLi = document.createElement("li");
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = "";
+    }
+  });
+}
+
+addNewTask();
