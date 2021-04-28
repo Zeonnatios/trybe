9d37202ff5a2a1361eaeee7e3956f92e7b248ba4
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-04-28 18:33:02 -0300

    Exercise 1

---
+++ Modulo_1/bloco_7/dia_3/exercises/part1/exercise_1.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(sum(4, 5), 9, 'A soma de 4 + 5 deve ser igual a 9!');
