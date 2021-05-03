802edc8cbbdd00fa68a4fb80174930ba907a418f
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-03 18:49:18 -0300

    Adicionado enunciados

--- Modulo_1/bloco_8/dia_1/exercise1.js
+++ Modulo_1/bloco_8/dia_1/exercise1.js
@@ -1,3 +1,13 @@
+/**
+ * Crie uma função de primeira classe que retorna 
+ * um objeto { nomeCompleto, email } de uma nova pessoa contratada. 
+ * Passe sua função como parâmetro da HOF newEmployees para criar 
+ * cada pessoa contratada em seu respctivo id . 
+ * A sua função deve receber como parâmetro o nome completo 
+ * da pessoa funcionária e a partir dele gerar automaticamente um 
+ * email no formato nome_da_pessoa@trybe.com .
+ */
+
 const generateEmployee = (fullName) => {
   const email = fullName.toLowerCase().split(' ').join('_');
   return { fullName, email: `${email}@trybe.com` };
