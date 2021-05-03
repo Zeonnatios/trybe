72485da8ac4da68e88aa3f286db38590d5bdfa5a
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-03 14:52:03 -0300

    Ex 2

--- Modulo_1/bloco_7/dia_2/exercicio_2.js
+++ Modulo_1/bloco_7/dia_2/exercicio_2.js
@@ -58,10 +58,10 @@ const getNumberOfStudents = (obj) => {
   let total = 0;
   const array = Object.keys(obj);
   for (index in array) {
-    if(obj[array[index]].materia === 'Matemática'){
-    total += obj[array[index]].numeroEstudantes;
+    if (obj[array[index]].materia === 'Matemática') {
+      total += obj[array[index]].numeroEstudantes;
     }
   }
   return total;
-}
+};
 console.log(getNumberOfStudents(allLessons));
