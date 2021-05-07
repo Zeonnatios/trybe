1f235257399daf4ecabbdbd9e203c3685f9ff0c0
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-07 19:39:26 -0300

    Finalizado exercicio 4

--- Modulo_1/bloco_8/dia_2/exercicios/exercicio_4.js
+++ Modulo_1/bloco_8/dia_2/exercicios/exercicio_4.js
@@ -109,7 +109,7 @@ const expectedResult = [
 ];
 
 function booksOrderedByReleaseYearDesc() {
-  // escreva aqui seu código
+  return books.sort((bookA, bookB) => bookB.releaseYear - bookA.releaseYear);
 }
 
 assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
