f1287151ad1364bb78b8a8c07fef3288a8b5050e
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-11 18:50:53 -0300

    Exercicios do dia finalizados

--- Modulo_1/bloco_9/dia_1/exercicios/Exercicio_4.js
+++ Modulo_1/bloco_9/dia_1/exercicios/Exercicio_4.js
@@ -5,7 +5,11 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-// crie a função sendMarsTemperature abaixo
-
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => {
+    console.log(`Mars temperature is: ${currentTemperature} degree Celsius`);
+  }, messageDelay());
+};
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
