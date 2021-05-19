091b9a904e0eb976c35e4bbfa660e161fe97c251
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-19 11:19:03 -0300

    Testado funçoes sum ;

--- Modulo_1/bloco_10/dia_1/jest/exercicios/parte_1/tests/sum.spec.js
+++ Modulo_1/bloco_10/dia_1/jest/exercicios/parte_1/tests/sum.spec.js
@@ -5,4 +5,20 @@ describe('test function sum', () => {
   test('sum(4,5) === 9', () => {
     expect(sum(4, 5)).toEqual(9);
   });
+
+  test('sum(0,0) === 0', () => {
+    expect(sum(0, 0)).toEqual(0);
+  });
+
+  test('throws on sum if params is a string (4, "5")', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+
+  test('message error is: "parameters must be numbers"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrowError(Error('parameters must be numbers'));
+  });
 });
