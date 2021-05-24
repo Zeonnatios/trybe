8f534337a1241c4ac06f7bcc7a32aaa903df4a12
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-24 14:02:11 -0300

    Fim dia 3

--- Modulo_1/bloco_10/dia_3/jestmock/jest.mock.spec.js
+++ Modulo_1/bloco_10/dia_3/jestmock/jest.mock.spec.js
@@ -14,3 +14,20 @@ test('#somar', () => {
   expect(math.somar).toHaveBeenCalledWith(1, 2);
   expect(math.somar(1, 2)).toBe(3);
 });
+
+/*
+  Há também como limpar, resetar ou restaurar mocks . Existem três métodos capazes de fazer isso:
+  
+  mock.mockClear()
+  Útil quando você deseja limpar os dados de uso de uma simulação entre dois expect s;
+  
+  mock.mockReset()
+  Faz o que o mockClear() faz;
+  Remove qualquer retorno estipulado ou implementação;
+  Útil quando você deseja resetar uma simulação para seu estado inicial;
+  
+  mock.mockRestore()
+  Faz tudo que mockReset() faz;
+  Restaura a implementação original;
+  Útil para quando você quer simular funções em certos casos de teste e restaurar a implementação original em outros;
+*/
