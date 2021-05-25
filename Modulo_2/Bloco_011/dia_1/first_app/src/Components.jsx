7eaa59edc095c0e0750217ad0c343d09b9b4ac18
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-24 22:14:05 -0300

    My first react-app

---
+++ Modulo_2/Bloco_011/dia_1/first_app/src/Components.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Components extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Matheus Antonio da Silva</h1>
+        <p>NickName: Zeonnatios, main Lass (F Grand Chase)</p>
+      </div>
+    );
+  }
+}
+
+export default Components;
