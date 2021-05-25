18b1dac1b8fea7ca0b7453513e5d48036f3248f2
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-25 14:45:55 -0300

    Renderizando elementos em pagina

--- Modulo_2/Bloco_011/dia_1/first_app/src/App.jsx
+++ Modulo_2/Bloco_011/dia_1/first_app/src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css';
-import Components from './Components'
+import React from 'react';
+import Components from './Components';
+import createTask from './createTask';
+
+const tech = ['React', 'NodeJs', 'JavaScript', 'HTML', 'CSS'];
 
 function App() {
-  return <Components />;
+  return (
+    <div>
+      <Components />
+      {tech.sort().map((element) => createTask(element))}
+    </div>
+  );
 }
 
 export default App;
