356b02c0f7fbb360b617611f6da4cae66aca8c6d
Matheus Antonio <matheusantonio232@gmail.com>
2022-01-18 15:52:04 -0300

    entendendo tuplas em python

---
+++ 4 - CS/Bloco_32/exercicios/tuples_example.py
@@ -0,0 +1,12 @@
+user = ("Cássio", "Botaro", 42)
+
+print(user[0])
+
+caelum_coordenadas = (-23.588254, -46.632477)
+print(type(caelum_coordenadas))
+print(caelum_coordenadas)
+
+tupla_list = (["a", "b", "c"], [1, 2, 3, 4])
+
+tupla_list[0].append("d")
+print(tupla_list)
