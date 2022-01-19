84a449593537cc26bffb4073a4ff2f3a359ffc3d
Matheus Antonio <matheusantonio232@gmail.com>
2022-01-19 12:17:39 -0300

    entendendo range em python

--- 4 - CS/Bloco_32/exercicios/exercicios.py
+++ 4 - CS/Bloco_32/exercicios/exercicios.py
@@ -1,3 +1,10 @@
 lista_dois = ["dois", "two", "dos", 2]
 for dois in lista_dois:
-    print(int(dois))
+    print((dois))
+
+user = ("Thiado", "Nobre", 29)
+print(user)
+
+my_list = [20, 20, 1, 2]
+
+count_elements = {20: 2, 1: 1, 2: 2}
