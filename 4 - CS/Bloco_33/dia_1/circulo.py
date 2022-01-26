3c80116843dffbe29249b1b8f39c851d4c026e8b
Matheus Antonio <matheusantonio232@gmail.com>
2022-01-26 11:12:38 -0300

    circulo - poo

---
+++ 4 - CS/Bloco_33/dia_1/circulo.py
@@ -0,0 +1,16 @@
+class Circulo:
+    PI = 3.14
+
+    def __init__(self, raio):
+        self.raio = raio
+
+    def calcular_area(self):
+        return self.PI * self.raio ** 2
+
+    def calcular_perimetro(self):
+        return 2 * self.PI * self.raio
+
+
+circulo_1 = Circulo(5)
+print(circulo_1.calcular_area())
+print(circulo_1.calcular_perimetro())
